110282fda244627f8b1fdebd58ebb8da9638b7d9
brolly <102495553@qq.com>
2022-08-25 21:42:54 +0800

    变更test

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import '@/index.less'
 import 'normalize.css'
 import Routes from '@components/Routes'
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
